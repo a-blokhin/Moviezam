5428b70203d63bbd36e3af788b8b6790f83bb9d9
moroz <MorozovaAnastasiaa2001@gmail.com>
2021-11-03 23:42:56 +0300

    basic commit

---
+++ app/src/main/java/com/example/moviezam/views/ui/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.moviezam.views.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.moviezam.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
