2b5a29f68166692a30c3883fbbd6416664d9259e
a-blokhin <blokhin.aa17@physics.msu.ru>
2021-11-17 16:20:26 +0500

    поправил MainActivity
    поправил MainActivity

--- app/src/main/java/com/example/moviezam/views/ui/MainActivity.kt
+++ app/src/main/java/com/example/moviezam/views/ui/MainActivity.kt
@@ -1,21 +1,18 @@
 package com.example.moviezam.views.ui
 
-
-import android.os.Build
-import android.os.Bundle
-import android.util.Log
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
+import android.os.Bundle
 import com.example.moviezam.R
+import androidx.fragment.app.Fragment
 import com.example.moviezam.models.Store
-import com.google.gson.Gson
-import java.nio.file.Paths.get
+import com.facebook.drawee.backends.pipeline.Fresco
 
-class MainActivity : AppCompatActivity(), BaseFragment.OnListFragmentInteractionListener  {
+
+class MainActivity : AppCompatActivity(), BaseFragment.OnListFragmentInteractionListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Fresco.initialize(this)
         setContentView(R.layout.activity_main)
 
         val shazamFragment: Fragment = ShazamFragment()
@@ -25,6 +22,7 @@ class MainActivity : AppCompatActivity(), BaseFragment.OnListFragmentInteraction
                 .commit()
         }
     }
+
     override fun onListFragmentInteraction(id: Int, f: BaseFragment) {
         Store.id = id
         supportFragmentManager
