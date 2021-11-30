189ed787a6a736112531c71643e5bd9a1b8d670d
moroz <MorozovaAnastasiaa2001@gmail.com>
2021-11-30 09:31:46 +0300

    icons paddings + one Moviezam Api for all

--- app/src/main/java/com/example/moviezam/repository/ArtistRepository.kt
+++ app/src/main/java/com/example/moviezam/repository/ArtistRepository.kt
@@ -2,33 +2,21 @@ package com.example.moviezam.repository
 
 import com.example.moviezam.models.Artist
 import com.example.moviezam.models.ArtistCard
-import com.example.moviezam.models.MoviezamApi
-import com.example.moviezam.models.Resource
+import com.example.moviezam.models.Moviezam
+
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.await
-import retrofit2.awaitResponse
-
-import retrofit2.converter.gson.GsonConverterFactory
 
 class ArtistRepository {
-    private val moviezamApi = retrofit.create(MoviezamApi::class.java)
 
     suspend fun getArtistById(id: Int): Response<Artist> {
-       return moviezamApi.getArtistById(id)
+       return Moviezam.api.getArtistById(id)
     }
 
     suspend fun getArtistsByName(name: String, page: Int): List<ArtistCard> = withContext(Dispatchers.IO) {
-        return@withContext moviezamApi.getArtistsByName(name, page).execute().body()
+        return@withContext Moviezam.api.getArtistsByName(name, page).execute().body()
             ?: emptyList()
     }
 
-    companion object {
-        private val retrofit = Retrofit.Builder()
-            .baseUrl("http://95.163.180.8:8081")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-    }
 }
--- app/src/main/java/com/example/moviezam/repository/FilmRepository.kt
+++ app/src/main/java/com/example/moviezam/repository/FilmRepository.kt
@@ -1,7 +1,5 @@
 package com.example.moviezam.repository
 
-import android.content.res.Resources
-import com.example.moviezam.R
 import com.example.moviezam.models.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -10,21 +8,14 @@ import retrofit2.*
 import retrofit2.converter.gson.GsonConverterFactory
 
 class FilmRepository {
-    private val moviezamApi = retrofit.create(MoviezamApi::class.java)
 
     suspend fun getFilmById(id: Int): Response<Film> {
-        return moviezamApi.getFilmById(id)
+        return Moviezam.api.getFilmById(id)
     }
 
     suspend fun getFilmsByName(name: String, page: Int): List<FilmCard> = withContext(Dispatchers.IO) {
-        return@withContext moviezamApi.getFilmsByName(name, page).execute().body()
+        return@withContext Moviezam.api.getFilmsByName(name, page).execute().body()
             ?: emptyList()
     }
 
-    companion object {
-        private val retrofit = Retrofit.Builder()
-            .baseUrl("http://95.163.180.8:8081")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-    }
 }
