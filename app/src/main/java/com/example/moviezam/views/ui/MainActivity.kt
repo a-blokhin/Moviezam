530055798f219cb80e4ddf3f76251b19a93f6036
Max <max21011999@mail.ru>
2021-12-11 22:57:48 +0300

    navmenu works

--- app/src/main/java/com/example/moviezam/views/ui/MainActivity.kt
+++ app/src/main/java/com/example/moviezam/views/ui/MainActivity.kt
@@ -2,18 +2,37 @@ package com.example.moviezam.views.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.moviezam.R
 import com.example.moviezam.models.*
 import com.example.moviezam.repository.SearchRepository
 import com.facebook.drawee.backends.pipeline.Fresco
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity(), BaseFragment.OnListFragmentInteractionListener {
 
+    private var mListener: BaseFragment.OnListFragmentInteractionListener? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         Fresco.initialize(this)
         setContentView(R.layout.activity_main)
+
+//        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+//        val navController = findNavController(R.id.nav_host_fragment)
+//        val appBarConfiguration = AppBarConfiguration(setOf(
+//            R.id.shazamFragment,
+//            R.id.searchFragment,
+//            R.id.favouriteFragment))
+////        setupActionBarWithNavController(navController, appBarConfiguration)
+//        navView.setupWithNavController(navController)
+
         AppDatabase.getInstance(this)
         SearchHistoryDatabase.getInstance(this)
 
@@ -22,6 +41,7 @@ class MainActivity : AppCompatActivity(), BaseFragment.OnListFragmentInteraction
             .beginTransaction()
             .replace(R.id.container, f)
             .commit()
+
     }
 
     override fun onListFragmentInteraction(id: Int, f: BaseFragment) {
